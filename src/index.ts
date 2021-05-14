fddf5c22b101db6ce4f6b318c900d33391d0d40d
frothywater <kokoroic@outlook.com>
2021-05-14 15:23:49 +0800

    feat: Basic Twitter setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello")
+import https from "https"
+import { SocksProxyAgent } from "socks-proxy-agent"
+import Twitter from "twitter"
+import token from "../secret/token.json"
+
+async function main() {
+    https.globalAgent = new SocksProxyAgent("socks://127.0.0.1:7891")
+
+    const twitter = new Twitter(token)
+    const result = await twitter.get("favorites/list", {})
+    console.log(result)
+}
+
+main()
