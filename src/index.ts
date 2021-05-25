e4145b57a3396dff9d7df2c1dbc7338a5a63db6b
frothywater <kokoroic@outlook.com>
2021-05-25 21:41:24 +0800

    chores: Library project structure

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import Database from "./Database"
 import Downloader from "./Downloader"
 import Network from "./Network"
 import Pixiv from "./Pixiv"
 import Twitter from "./Twitter"
 
-async function main() {
-    Network.useProxy()
-    await Database.init()
-
-    const twitter = new Twitter()
-    await twitter.updateFavorites()
-    const pixiv = new Pixiv()
-    await pixiv.init()
-    await pixiv.updateFavorites()
-
-    const twitterTasks = await Twitter.getNotDownloadedTask()
-    const pixivTasks = await Pixiv.getNotDownloadedTask()
-    const downloader = new Downloader()
-    await downloader.run(twitterTasks.concat(pixivTasks))
-}
-
-main()
+export { Database, Downloader, Network, Pixiv, Twitter }
--- test/index.ts
+++ test/index.ts
@@ -1,1 +1,19 @@
-console.log("Hello")
+import { Database, Downloader, Network, Pixiv, Twitter } from "../src"
+
+async function main() {
+    Network.useProxy()
+    await Database.init()
+
+    const twitter = new Twitter()
+    await twitter.updateFavorites()
+    const pixiv = new Pixiv()
+    await pixiv.init()
+    await pixiv.updateFavorites()
+
+    const twitterTasks = await Twitter.getNotDownloadedTask()
+    const pixivTasks = await Pixiv.getNotDownloadedTask()
+    const downloader = new Downloader()
+    await downloader.run(twitterTasks.concat(pixivTasks))
+}
+
+main()
