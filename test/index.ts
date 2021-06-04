1f5689a8da2232ad7e4745950d9a45586ed60ff1
frothywater <kokoroic@outlook.com>
2021-06-04 17:56:13 +0800

    refactor: Organize different tests

--- test/index.ts
+++ test/index.ts
@@ -1,20 +1,49 @@
-import { Database, Network, Twitter } from "../src"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Database, Downloader, Network, Pixiv, Twitter } from "../src"
 
-async function main() {
+async function init() {
     Network.useProxy()
     await Database.init()
+}
 
+async function updateTwitter() {
     const twitter = new Twitter()
-    // await twitter.updateFavorites()
-    // const pixiv = new Pixiv()
-    // await pixiv.init()
-    // await pixiv.updateFavorites()
+    await twitter.updateFavorites()
+    const another = new Twitter("frothywater1")
+    await another.updateFavorites()
+}
 
-    // const twitterTasks = await Twitter.getNotDownloadedTask()
-    // const pixivTasks = await Pixiv.getNotDownloadedTask()
-    // const downloader = new Downloader()
-    // await downloader.run(twitterTasks.concat(pixivTasks))
+async function updatePixiv() {
+    const pixiv = new Pixiv()
+    await pixiv.init()
+    await pixiv.updateFavorites()
+}
+
+async function downloadTwitter() {
+    const twitterTasks = await Twitter.getNotDownloadedTask()
+    const downloader = new Downloader()
+    await downloader.run(twitterTasks)
+}
+
+async function downloadPixiv() {
+    const downloader = new Downloader()
+    const pixivTasks = await Pixiv.getNotDownloadedTask()
+    await downloader.run(pixivTasks)
+}
+
+async function postUnlikedTweets() {
+    await init()
+    const twitter = new Twitter()
+    await twitter.updateFavorites()
     await twitter.postUnlikedTweets()
+}
+
+async function pixivStat() {
+    await Pixiv.performStat()
+}
+
+async function main() {
+    await init()
 }
 
 main()
